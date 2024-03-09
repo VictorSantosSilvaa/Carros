429dd1770e9c0c05303751e690f90ae6d71cd575
Victor dos Santos da Silva <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-03-09 16:49:48 -0300

    update

--- src/HotWeels/Menu.java
+++ src/HotWeels/Menu.java
@@ -3,24 +3,62 @@ package HotWeels;
 import java.util.Scanner;
 
 public class Menu {
-	static Scanner sc = new Scanner(System.in);
 
 	public static void menu() {
-		System.out.println("");
-		System.out.println("------------------------------------");
-		System.out.println("Olá, seja Bem-Vindo ao Sistema de Veiculos.");
-		System.out.println("------------------------------------");
-		System.out.println("------------------------------------");
-		System.out.println("Selecione o numero que deseja:");
-		System.out.println("------------------------------------");
-		System.out.println("------------------------------------");
-		System.out.println("1 - Carro.");
-		System.out.println("2 - Moto.");
-		System.out.println("3 - Ver Lista de Carro.");
-		System.out.println("4 - Ver Lista de Moto.");
-		System.out.println("9 - Sair do sistema.");
-		System.out.println("------------------------------------");
-		System.out.println("");
-		System.out.print("Digite um numero: ");
+		do {
+			Scanner sc = new Scanner(System.in);
+			try {
+				System.out.println("");
+				System.out.println("------------------------------------");
+				System.out.println("Olá, seja Bem-Vindo ao Sistema de Veiculos.");
+				System.out.println("------------------------------------");
+				System.out.println("------------------------------------");
+				System.out.println("Selecione o numero que deseja:");
+				System.out.println("------------------------------------");
+				System.out.println("------------------------------------");
+				System.out.println("1 - Carro.");
+				System.out.println("2 - Moto.");
+				System.out.println("3 - Ver Lista de Carro.");
+				System.out.println("4 - Ver Lista de Moto.");
+				System.out.println("9 - Sair do sistema.");
+				System.out.println("------------------------------------");
+				System.out.println("");
+				System.out.print("Digite um numero: ");
+
+				int opcao = sc.nextInt();
+
+				switch (opcao) {
+				case 1: {
+					Carro.carro();
+					continue;
+				}
+				case 2: {
+					Moto.moto();
+					continue;
+				}
+				case 3: {
+					Carro.verCarro();
+					continue;
+				}
+				case 4: {
+					Moto.verMoto(null);
+
+				}
+				case 9: {
+					System.out.println("Você saiu do sistema.");
+					return;
+				}
+				default:
+					System.out.println("Algo deu errado.");
+
+				}
+			} catch (Exception e) {
+				System.out.println("Algo deu errado mais uma vez.");
+
+			}
+
+		} while (true);
+
 	}
+
 }
--- src/HotWeels/Principal.java
+++ src/HotWeels/Principal.java
@@ -1,49 +1,10 @@
 package HotWeels;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Moto moto = new Moto();
-		do {
-			try {
-
-				Menu.menu();
-				int opcao = sc.nextInt();
-
-				switch (opcao) {
-				case 1: {
-					Carro.carro();
-					continue;
-				}
-				case 2: {
-					Moto.moto();
-					continue;
-				}
-				case 3: {
-					Carro.verCarro();
-					continue;
-				}
-				case 4: {
-					Moto.verMoto(null);
-
-				}
-				case 9: {
-					System.out.println("Você saiu do sistema.");
-					return;
-				}
-				default:
-					System.out.println("Algo deu errado.");
-
-				}
-			} catch (Exception e) {
-				System.out.println("Algo deu errado mais uma vez.");
-			}
-		} while (true);
-
+		
+		Menu.menu();
 	}
 
 }
